254e0068d30def7cb150e5c6ebe7b62fe72309b9
Traoré225 <siaka2moussa@gmail.com>
2021-05-04 16:11:26 +0000

    fourth commit

---
+++ data-raw/bonds_CIV.R
@@ -0,0 +1,14 @@
+## code to prepare `bonds_CIV` dataset goes here
+require(dplyr)
+require(DBI)
+
+connect2 <- DBI::dbConnect(odbc::odbc(), "MyCON")
+a = DBI::dbSendQuery(connect2, "SELECT pays.nompays, pays.nombourse, pays.devise, 
+                                obligation.code_obligation, obligation.num_jourO, 
+                                obligation.ValeurOb, obligation.VariationOb, 
+                                obligation.VolumeOb
+                                FROM obligation
+                                INNER JOIN pays ON obligation.numpays = pays.numpays
+                                WHERE obligation.numpays = 1;")
+bonds_CIV <- DBI::dbFetch(a) %>% dplyr::tibble()
+usethis::use_data(bonds_CIV, overwrite = TRUE)
